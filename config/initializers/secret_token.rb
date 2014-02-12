e59241e04ad777417859a64a75734715755947d5
sapao <mutskxabaa@gmail.com>
2014-02-12 18:58:36 +0000

    primer commit de mi nuevo blog en rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NerdBlog::Application.config.secret_key_base = '73a31a491bda67b69f6352ddb7293e4d6ca654eec19eab519d060893aa0c583ad64abfff3d6a135181c6030ae562eecd1f51184a871ed06463cdda37ea3f030c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NerdBlog::Application.config.session_store :cookie_store, key: '_nerd_blog_session'
